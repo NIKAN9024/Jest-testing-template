e9462fc8922660bce15b0051cbbd722d8d64493e
NIKAN9024 <9024387@student.tcrmbo.nl>
2026-04-12 22:11:57 +0200

    ....

--- src/opdracht-04/ShoppingCart.test.js
+++ src/opdracht-04/ShoppingCart.test.js
@@ -5,7 +5,9 @@ describe('ShoppingCart', () => {
 
   beforeEach(() => {
     // TODO: Maak hier een nieuwe ShoppingCart aan
+       // Deze regel maakt een nieuwe cart aan voor elke test
     // Hint: cart = new ShoppingCart();
+    cart = new ShoppingCart();
   });
 
   // VOORBEELD - Deze test is al ingevuld
@@ -16,34 +18,44 @@ describe('ShoppingCart', () => {
 
   test('addItem voegt product toe', () => {
     // TODO: Voeg een product toe: { id: 1, name: 'Laptop', price: 999 }
+     cart.addItem({ id: 1, name: 'Laptop', price: 999 });
     // TODO: Check of getItemCount() nu 1 is
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    
+    expect(cart.getItemCount()).toBe(1); // Deze test faalt! Vervang met je eigen test
   });
 
   test('addItem met quantity voegt meerdere items toe', () => {
     // TODO: Voeg product toe met quantity 3
     // TODO: Check of getItemCount() 3 is
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    cart.addItem({ id: 2, name: 'Muis', price: 25 }, 3);
+    expect(cart.getItemCount()).toBe(3); // Deze test faalt! Vervang met je eigen test
   });
 
   test('getTotal berekent juiste totaal', () => {
     // TODO: Voeg product toe: { id: 1, name: 'Muis', price: 25 }, quantity 2
+    cart.addItem({ id: 1, name: 'Muis', price: 25 }, 2);
     // TODO: Check of getTotal() 50 teruggeeft (2 x 25)
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    expect(cart.getTotal()).toBe(50); // Deze test faalt! Vervang met je eigen test
   });
 
   test('removeItem verwijdert product', () => {
     // TODO: Voeg een product toe met id: 1
+     cart.addItem({ id: 1, name: 'Laptop', price: 999 });
     // TODO: Roep removeItem(1) aan
+     cart.removeItem(1);
     // TODO: Check of getItemCount() 0 is
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    expect(cart.getItemCount()).toBe(0); // Deze test faalt! Vervang met je eigen test
   });
 
   test('clear maakt cart leeg', () => {
     // TODO: Voeg 2 producten toe
+     cart.addItem({ id: 1, name: 'Laptop', price: 999 });
+     cart.addItem({ id: 2, name: 'Muis', price: 25 }, 3);
     // TODO: Roep clear() aan
+    cart.clear();
     // TODO: Check of getItemCount() 0 is
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    expect(cart.getItemCount()).toBe(0);
+    // Deze test faalt! Vervang met je eigen test
   });
 
 });
